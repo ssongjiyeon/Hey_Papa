a0db2b6248428c5dc1fa199e3fa2fba07f811df9
whgywjd123 <whgywjd6794@gmail.com>
2021-10-19 11:15:42 +0900

    S05P31B206-84 [feat]: [BE]backend 스켈레톤 코드

---
+++ heypapa/heypapa/src/main/java/com/ssafy/heypapa/HeypapaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.heypapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeypapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeypapaApplication.class, args);
+	}
+
+}
---
+++ heypapa/heypapa/src/test/java/com/ssafy/heypapa/HeypapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.heypapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeypapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
