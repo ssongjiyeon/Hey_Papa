01d964803a59645e55a393eb3bd3fdfd9761c9c9
Jiyeon526 <b8480@naver.com>
2021-11-01 13:31:47 +0900

    S05P31B206-144 [fix]: [BE] 주석 추가

--- backend/heypapa/src/main/java/com/ssafy/heypapa/auth/JwtRequestFilter.java
+++ backend/heypapa/src/main/java/com/ssafy/heypapa/auth/JwtRequestFilter.java
@@ -23,81 +23,81 @@ import com.ssafy.heypapa.util.JwtTokenUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 
 @Component
-public class JwtRequestFilter extends OncePerRequestFilter {
+public class JwtRequestFilter {// extends OncePerRequestFilter {
 	
-	@Autowired
-	private PapaUserDetailService userService;
-	
-	@Autowired
-	private JwtTokenUtil jwtUtil;
-	
-	@Autowired
-	private CookieUtil cookieUtil;
-
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
+//	@Autowired
+//	private PapaUserDetailService userService;
+//	
+//	@Autowired
+//	private JwtTokenUtil jwtUtil;
+//	
+//	@Autowired
+//	private CookieUtil cookieUtil;
+//
+//	@Override
+//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//			throws ServletException, IOException {
+//		
+//		final Cookie jwtToken = cookieUtil.getCookie(request, JwtTokenUtil.ACCESS_TOKEN_NAME);
+//		
+//		String nickname = null;
+//		String jwt = null;
+//		String refreshJwt = null;
+//		String refreshNickname = null;
+//		
+//		try {
+//			
+//			if(jwtToken != null) {
+//				jwt = jwtToken.getValue();
+//				nickname = jwtUtil.getUserNickname(jwt);
+//			}
+//			
+//			if(nickname != null) {
+//				UserDetails userDetails = userService.loadUserByUsername(nickname);
+//				
+//				if(jwtUtil.validateToken(jwt, userDetails)) {
+//					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+//	                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//					
+//					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//				}
+//			}
+//			
+//		} catch(ExpiredJwtException e) {
+//			Cookie refreshToken = cookieUtil.getCookie(request, JwtTokenUtil.REFRESH_TOKEN_NAME);
+//			
+//			if(refreshToken != null) {
+//				refreshJwt = refreshToken.getValue();
+//			}
+//		} catch(Exception e) {
+//			
+//		}
 		
-		final Cookie jwtToken = cookieUtil.getCookie(request, JwtTokenUtil.ACCESS_TOKEN_NAME);
+//		try {
+//			if(refreshJwt != null) {
+////				refreshNickname = redisUtil.getData(refreshJwt);
+//				
+//				if(refreshNickname.equals(jwtUtil.getUserNickname(refreshJwt))) {
+//					UserDetails userDetails = userService.loadUserByUsername(refreshNickname);
+//					UsernamePasswordAuthenticationToken upaToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+//					upaToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//					SecurityContextHolder.getContext().setAuthentication(upaToken);
+//					
+//					User user = new User();
+//					user.setNickname(refreshNickname);
+//					String newToken = jwtUtil.generateToken(user);
+//					
+//					Cookie newAccessToken = cookieUtil.createCookie(JwtTokenUtil.ACCESS_TOKEN_NAME, newToken);
+//					response.addCookie(newAccessToken);
+//				}
+//			}
+//		} catch(ExpiredJwtException e){
+//
+//        }
 		
-		String nickname = null;
-		String jwt = null;
-		String refreshJwt = null;
-		String refreshNickname = null;
+//		filterChain.doFilter(request, response);
 		
-		try {
-			
-			if(jwtToken != null) {
-				jwt = jwtToken.getValue();
-				nickname = jwtUtil.getUserNickname(jwt);
-			}
-			
-			if(nickname != null) {
-				UserDetails userDetails = userService.loadUserByUsername(nickname);
-				
-				if(jwtUtil.validateToken(jwt, userDetails)) {
-					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-	                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-					
-					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-				}
-			}
-			
-		} catch(ExpiredJwtException e) {
-			Cookie refreshToken = cookieUtil.getCookie(request, JwtTokenUtil.REFRESH_TOKEN_NAME);
-			
-			if(refreshToken != null) {
-				refreshJwt = refreshToken.getValue();
-			}
-		} catch(Exception e) {
-			
-		}
-		
-		try {
-			if(refreshJwt != null) {
-				refreshNickname = redisUtil.getData(refreshJwt);
-				
-				if(refreshNickname.equals(jwtUtil.getUserNickname(refreshJwt))) {
-					UserDetails userDetails = userService.loadUserByUsername(refreshNickname);
-					UsernamePasswordAuthenticationToken upaToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-					upaToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-					SecurityContextHolder.getContext().setAuthentication(upaToken);
-					
-					User user = new User();
-					user.setNickname(refreshNickname);
-					String newToken = jwtUtil.generateToken(user);
-					
-					Cookie newAccessToken = cookieUtil.createCookie(JwtTokenUtil.ACCESS_TOKEN_NAME, newToken);
-					response.addCookie(newAccessToken);
-				}
-			}
-		} catch(ExpiredJwtException e){
-
-        }
-		
-		filterChain.doFilter(request, response);
-		
-	}
+//	}
 	
 	
 }
--- backend/heypapa/src/main/java/com/ssafy/heypapa/auth/PapaUserDetails.java
+++ backend/heypapa/src/main/java/com/ssafy/heypapa/auth/PapaUserDetails.java
@@ -10,63 +10,63 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.ssafy.heypapa.entity.User;
 
-public class PapaUserDetails implements UserDetails {
+public class PapaUserDetails {//implements UserDetails {
 
-	@Autowired
-	User user;
-	
-	boolean accountNonExpired;
-	boolean accountNonLocked;
-	boolean credentailNonExpired;
-	boolean enabled = false;
-	List<GrantedAuthority> roles = new ArrayList<>();
-	
-	public PapaUserDetails(User user) {
-		super();
-		this.user = user;
-	}
-	
-	public User getUser() {
-		return this.user;
-	}
-	
-	public void setAuthorities(List<GrantedAuthority> roles) {
-		this.roles = roles;
-	}
-	
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.roles;
-	}
-
-	@Override
-	public String getPassword() {
-		return this.user.getPassword();
-	}
-
-	@Override
-	public String getUsername() {
-		return this.user.getNickname();
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return this.accountNonExpired;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return this.accountNonLocked;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return this.credentailNonExpired;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return this.enabled;
-	}
+//	@Autowired
+//	User user;
+//	
+//	boolean accountNonExpired;
+//	boolean accountNonLocked;
+//	boolean credentailNonExpired;
+//	boolean enabled = false;
+//	List<GrantedAuthority> roles = new ArrayList<>();
+//	
+//	public PapaUserDetails(User user) {
+//		super();
+//		this.user = user;
+//	}
+//	
+//	public User getUser() {
+//		return this.user;
+//	}
+//	
+//	public void setAuthorities(List<GrantedAuthority> roles) {
+//		this.roles = roles;
+//	}
+//	
+//	@Override
+//	public Collection<? extends GrantedAuthority> getAuthorities() {
+//		return this.roles;
+//	}
+//
+//	@Override
+//	public String getPassword() {
+//		return this.user.getPassword();
+//	}
+//
+//	@Override
+//	public String getUsername() {
+//		return this.user.getNickname();
+//	}
+//
+//	@Override
+//	public boolean isAccountNonExpired() {
+//		return this.accountNonExpired;
+//	}
+//
+//	@Override
+//	public boolean isAccountNonLocked() {
+//		return this.accountNonLocked;
+//	}
+//
+//	@Override
+//	public boolean isCredentialsNonExpired() {
+//		return this.credentailNonExpired;
+//	}
+//
+//	@Override
+//	public boolean isEnabled() {
+//		return this.enabled;
+//	}
 
 }
