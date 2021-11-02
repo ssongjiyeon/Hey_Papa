9464834256f525259cd7a00a64d35b0188125bfc
박선주 <cucu9823@naver.com>
2021-11-02 13:03:18 +0900

    feat :[FE] 조리원마커추가

--- frontend/src/store/module/actions.js
+++ frontend/src/store/module/actions.js
@@ -1,2 +1,7 @@
-export function someAction (/* context */) {
+import $axios from 'axios'
+const baseUrl = 'https://k5b206.p.ssafy.io/api'
+
+export function getCarecenter ({}) {
+  const url = baseUrl + '/Carecenter'
+  return $axios.get(url)
 }
