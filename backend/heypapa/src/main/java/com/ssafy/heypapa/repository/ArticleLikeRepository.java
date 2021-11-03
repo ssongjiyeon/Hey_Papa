6c057869c4d76b45b13647516bfcff14c73a5d98
whgywjd123 <whgywjd6794@gmail.com>
2021-11-03 10:55:05 +0900

    S05P31B206-237 [feat]: [BE] 좋아요 구현

---
+++ backend/heypapa/src/main/java/com/ssafy/heypapa/repository/ArticleLikeRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.heypapa.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ssafy.heypapa.entity.Article;
+import com.ssafy.heypapa.entity.ArticleLike;
+
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long>{
+	List<Article> findByArticleId(Long id);
+	Optional<ArticleLike> findByArticleIdAndUserId(Long articlrId, Long userId);
+}
