6db70d14c9b014092d8399fec7763b21924a65a3
Jiyeon526 <b8480@naver.com>
2021-11-04 01:08:02 +0900

    S05P31B206-145 [fix]: [BE] 주석처리(다시 구현해야된다.)

--- backend/heypapa/src/main/java/com/ssafy/heypapa/auth/PapaUserDetailService.java
+++ backend/heypapa/src/main/java/com/ssafy/heypapa/auth/PapaUserDetailService.java
@@ -7,19 +7,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import com.ssafy.heypapa.entity.User;
-import com.ssafy.heypapa.service.IUserService;
+import com.ssafy.heypapa.service.UserService;
+
 
 @Component
 public class PapaUserDetailService implements UserDetailsService {
 
 	@Autowired
-	IUserService userService;
+	UserService userService;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userService.getUserByNickname(username);
 		
 		if(user != null) {
+			System.out.println("papaService = " + user.getEmail());
 			PapaUserDetails userDetails = new PapaUserDetails(user);
 			return userDetails;
 		}
