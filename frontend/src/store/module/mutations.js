84034043aaf390698bcec1f7983013bcb23ee8c5
천수승 <cheonss2357@gmail.com>
2021-11-03 17:30:54 +0900

    #S05P31B206-262 [feature] : [FE] quiz-api 연결

--- frontend/src/store/module/mutations.js
+++ frontend/src/store/module/mutations.js
@@ -1,6 +1,10 @@
-export function setUser (state, user) {
-  state.user = user
+export function setUser(state, user) {
+  state.user = user;
 }
-export function setPwd (state, pwd) {
-  state.loginPwd = pwd
+export function setPwd(state, pwd) {
+  state.loginPwd = pwd;
+}
+export function quizList(state, quizList) {
+  state.quizList = quizList;
+  // console.log(state.quizList, "stateQL");
 }
--- frontend/src/store/module/state.js
+++ frontend/src/store/module/state.js
@@ -1,41 +1,43 @@
 export default function () {
   const myArticle = [
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
-  ]
+  ];
   const myLike = [
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
     {
-      img:'http://placehold.it/130x130'
+      img: "http://placehold.it/130x130",
     },
-  ]
-  const user = {}
-  const loginPwd = ''
+  ];
+  const user = {};
+  const loginPwd = "";
+  const quizList = [];
   return {
     myArticle,
     myLike,
     user,
-    loginPwd
-  }
+    loginPwd,
+    quizList,
+  };
 }
