a11958c48f2dc8ea03f59c815e49dc61b6003c2c
Jiyeon526 <b8480@naver.com>
2021-11-04 10:42:42 +0900

    S05P31B206-235 [feat, fix]: [BE] 회원 정보 조회 및 수정

--- backend/heypapa/src/main/java/com/ssafy/heypapa/service/IUserService.java
+++ backend/heypapa/src/main/java/com/ssafy/heypapa/service/IUserService.java
@@ -4,12 +4,14 @@ import com.ssafy.heypapa.entity.User;
 import com.ssafy.heypapa.request.RegistRequest;
 import com.ssafy.heypapa.request.UserModifyRequest;
 import com.ssafy.heypapa.request.UserRequest;
+import com.ssafy.heypapa.response.ProfileResponse;
 
 public interface IUserService {
 
 	User getUserByNickname(String username);
 	User getUserByEmail(String email);
 	User createUser(RegistRequest req);
-	boolean putUser(User user, UserModifyRequest req);
+	boolean putUser(long userId, UserModifyRequest req);
+	ProfileResponse getProfile(long userId);
 	
 }
