7c28fd0a3faf9bf6402f3f92c13808a4d0d108d2
Jiyeon526 <b8480@naver.com>
2021-11-08 14:06:40 +0900

    [fix]: 좋아요 수정

--- backend/heypapa/src/main/java/com/ssafy/heypapa/repository/ArticleLikeRepository.java
+++ backend/heypapa/src/main/java/com/ssafy/heypapa/repository/ArticleLikeRepository.java
@@ -12,4 +12,5 @@ public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long>{
 	List<ArticleLike> findByArticleId(Long id);
 	Optional<ArticleLike> findByArticleIdAndUserId(Long articlrId, Long userId);
 	List<ArticleLike> findByUserId(long userId);
+	ArticleLike findByUserIdAndArticleId(long userId, Long id);
 }
